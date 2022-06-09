a52eaa90c44de7157ca7f934bb0fabfc5e1bb62f
Tracy Derraco <ecert31@gmail.com>
2022-06-08 18:33:05 -0700

    save notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,28 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const termData = require('./db/db.json');
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//returns the notes.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-//get is read
-//post is create
-//update updates
-// delete deletes
-
-
-app.get('/api/notes', (req, res) => res.json(termData));
+//saves notes and joins db json
+app.get('/api/notes', (req, res) => {
+   return res.json( fs.readFile('db/db.json','utf-8').then(saveNotes => {
+        return JSON.parse(saveNotes)
+    }))
+});
+//returns index.html
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
- app.post('/api/notes', (req, res) => {
-     termData.push(req.body)
-     res.json(termData)
- } )
+//add notes to db json
+app.post('/api/notes', (req, res) => {
+    termData.push(req.body)
+    res.json(termData)
+})
 
 app.listen(PORT, function () {
     console.log('app is listening PORT ' + PORT)
