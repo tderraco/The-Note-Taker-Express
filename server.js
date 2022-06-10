0776be03f747e7a30e55c15d69fb19e2dff859e0
Tracy Derraco <ecert31@gmail.com>
2022-06-09 17:33:53 -0700

    update the server.js move

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const termData = require('./Develop/db/db.json');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//returns the notes.html
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
+//saves notes and joins db json
+app.get('/api/notes', (req, res) => res.json(termData));
+//returns index.html
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
+//add notes to db json
+app.post('/api/notes', (req, res) => {
+    termData.push(req.body)
+    res.json(termData)
+})
+
+app.listen(PORT, function () {
+    console.log('app is listening PORT ' + PORT)
+})
