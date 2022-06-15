0086a7925006d97ea99e4c7eb8a59b1a5dc6852c
Tracy Derraco <ecert31@gmail.com>
2022-06-14 22:33:51 -0700

    removed doubled files and reset file paths

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const termData = require('./Develop/db/db.json');
+const termData = require('./db/db.json');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,11 +9,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //returns the notes.html
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 //saves notes and joins db json
 app.get('/api/notes', (req, res) => res.json(termData));
 //returns index.html
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 //add notes to db json
 app.post('/api/notes', (req, res) => {
     termData.push(req.body)
